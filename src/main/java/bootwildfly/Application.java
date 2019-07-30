a0e752da040ce72b929e41df557280466365995e
A66155281 <A66155281@BE1AM707.emea1.cds.t-internal.com>
2019-07-30 10:43:43 +0200

    Changed ComponentScan and added Beanname Printout

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -5,15 +5,22 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages="['amqp']")
+@ComponentScan(basePackages="amqp")
 public class Application extends SpringBootServletInitializer {
-
+	private static ApplicationContext applicationContext;
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        applicationContext = SpringApplication.run(Application.class, args);
+        
+        String[] beanNames = applicationContext.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
     
 
