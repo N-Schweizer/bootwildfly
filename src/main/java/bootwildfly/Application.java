3464f7cac5efa5aeb5ea8b28e7cfa162421a120a
A66155281 <A66155281@BE1AM707.emea1.cds.t-internal.com>
2019-07-30 10:29:12 +0200

    Added Basepackeges

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages="['amqp']")
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
